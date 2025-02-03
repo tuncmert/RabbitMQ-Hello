c9d62a62bce6a7606736c995ed5f8e2dd421d8d3
mert <merttunc20@gmail.com>
2025-02-03 17:07:18 +0300

    Add project files.

---
+++ RabbitMQ.consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://xoagscos:E7j0IsuTnacByY-oAYIPcoQzQyabVU9W@shark.rmq.cloudamqp.com/xoagscos")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+channel.QueueDeclareAsync("hello-queue", true, false, false);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+await channel.BasicConsumeAsync("hello-queue", true, consumer);
+
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Gelen mesaj: " + message);
+    return Task.CompletedTask;
+};
+
+
+
+Console.ReadLine();
---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://xoagscos:E7j0IsuTnacByY-oAYIPcoQzQyabVU9W@shark.rmq.cloudamqp.com/xoagscos")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+channel.QueueDeclareAsync("hello-queue", true ,false, false);
+
+string message = "Hello World";
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(string.Empty, "hello-queue", messageBody);
+
+Console.WriteLine("Mesaj gönderildi");
+Console.ReadLine();
